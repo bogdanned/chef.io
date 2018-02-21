79135e9aa65ade71734dd2b92ec1621fabf8bb48
bogdanned <bogdan.nedelcu@lokallead.de>
2018-02-21 16:23:36 +0100

    hardwarde

--- ratatouille/src/App.js
+++ ratatouille/src/App.js
@@ -4,8 +4,50 @@ import './App.css';
 import Step from './step.js';
 import Menu from './menu.js';
 import { Icon, Grid } from 'semantic-ui-react'
+import styled from 'styled-components'
+import {ThingsClient} from "./things-client.js";
+
+
+const Card = styled.div`
+  width: 200px:
+  height: 100px;
+`
+
+const username = 'bcx18'
+const password = 'bcx18!Open2'
+const apitoken = 'db7f4e0cca344d32be72914311f1055f'
+const thingid = 'bosch.xdk:9677286257312676'
+const topic = 'bosch.xdk/myXDK/things/twin/events/modified'
+
+function prettyPrintJson(json) {
+  return JSON.stringify(json, undefined, 2);
+}
+
+function tryParseJson(message) {
+  try {
+      return JSON.parse(message);
+  } catch (e) {
+      return message;
+  }
+}
+
 
 class App extends Component {
+  async componentWillMount(){
+    const client = new ThingsClient((message) => {
+      let json = tryParseJson(message);
+      if (json && topic) {
+          if (json.topic !== topic) {
+              return;
+          }
+      }
+      console.log(json)
+    })
+    
+    await client.connect(username, password, apitoken);
+    await client.retrieve(username, password, apitoken, thingid)
+    
+  }
   render() {
     return (
       <div className="App">
@@ -29,7 +71,7 @@ class App extends Component {
         </Grid.Row>
         <Grid.Row>
         <Step></Step>
-
+          <Card></Card>
         </Grid.Row>
       </Grid>
 
